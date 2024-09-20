a5b2454e2ae51b1fa4d636bf2e296a03177d7c28
Javier Rosales <javier.rb_96@hotmail.com>
2024-09-20 12:08:05 +0200

    Add thread safety and code coverage

--- app/services/car_pooling_service.rb
+++ app/services/car_pooling_service.rb
@@ -2,45 +2,61 @@ class CarPoolingService
   attr_reader :cars, :groups
 
   def initialize
-    @cars = {}   # Hash table for cars: car_id => { seats: int, available_seats: int }
-    @groups = {} # Hash table for groups: group_id => { people: int, car_id: int or nil }
+    @cars = {}         # Hash map car_id => { seats: int, available_seats: int }
+    @groups = {}       # Hash map group_id => { people: int, car_id: int or nil }
+    @mutex = Mutex.new # Mutex for thread safety
   end
 
   def load_cars(car_list)
-    @cars.clear
-    @groups.clear
+    @mutex.synchronize do
+      @cars.clear
+      @groups.clear
 
-    car_list.each { |car| @cars[car[:id].to_i] = { id: car[:id].to_i, seats: car[:seats].to_i, available_seats: car[:seats].to_i } }
+      car_list.each do |car|
+        seats = car[:seats].to_i
+        next if seats < 4 || seats > 6
+
+        @cars[car[:id].to_i] = { id: car[:id].to_i, seats: seats, available_seats: seats }
+      end
+    end
   end
 
   def add_group(group_id, people_count)
-    return false if people_count < 1 || people_count > 6 || @groups.key?(group_id)
+    @mutex.synchronize do
+      return false if people_count < 1 || people_count > 6 || @groups.key?(group_id)
 
-    group = { id: group_id, people: people_count }
-    assign_group_to_car(group)
-    true
+      group = { id: group_id, people: people_count }
+      assign_group_to_car(group)
+      true
+    end
   end
 
   def dropoff_group(group_id)
-    group = @groups.delete(group_id)
-    if group.present?
-      if group[:car_id].present?
-        @cars[group[:car_id]][:available_seats] += group[:people]
-        @groups.each_value { |group| assign_group_to_car(group) if group[:car_id].nil? }
+    @mutex.synchronize do
+      group = @groups.delete(group_id)
+      if group.present?
+        if group[:car_id].present?
+          @cars[group[:car_id]][:available_seats] += group[:people]
+          @groups.each_value { |wgroup| assign_group_to_car(wgroup) if wgroup[:car_id].nil? }
+        end
+        true
+      else
+        false
       end
-      true
-    else
-      false
     end
   end
 
   def locate_group(group_id)
-    group = @groups[group_id]
-    @cars[group[:car_id]] if group.present? && group[:car_id].present?
+    @mutex.synchronize do
+      group = @groups[group_id]
+      @cars[group[:car_id]] if group.present? && group[:car_id].present?
+    end
   end
 
   def group_waiting?(group_id)
-    @groups[group_id] && @groups[group_id][:car_id].nil?
+    @mutex.synchronize do
+      @groups.key?(group_id) && @groups[group_id][:car_id].nil?
+    end
   end
 
   private
--- test/services/car_pooling_service_test.rb
+++ test/services/car_pooling_service_test.rb
@@ -38,7 +38,7 @@ class CarPoolingServiceTest < ActiveSupport::TestCase
     assert_equal({ id: 3, people: 3, car_id: 1 }, @service.groups[3])
   end
 
-  test 'add and remove group validations' do
+  test 'add and remove group correctly' do
     assert_equal(true, @service.add_group(1, 4))
     assert_equal(false, @service.add_group(1, 4))
     assert_equal(@service.cars[1], @service.locate_group(1))
@@ -48,5 +48,23 @@ class CarPoolingServiceTest < ActiveSupport::TestCase
 
     assert_equal(false, @service.add_group(2, 7))
     assert_nil(@service.locate_group(2))
+  end
+
+  test 'service with thread safety' do
+    car_list = (1..10_000).map { |id| { id: id, seats: rand(4..6) } }
+    @service.load_cars(car_list)
+
+    threads = []
+    10.times do
+      threads << Thread.new do
+        10_000.times { |id| @service.add_group(id, rand(1..6)) }
+      end
+    end
+    threads.each(&:join)
+    assert_equal(10_000, @service.groups.size)
+
+    sum_seats = @service.cars.values.map { |c| c[:seats] - c[:available_seats] }.sum
+    sum_people = @service.groups.values.map { |g| g[:car_id] ? g[:people] : 0 }.sum
+    assert_equal(sum_seats, sum_people)
   end
 end
